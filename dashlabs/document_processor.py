02f43ff761f1d6b7a8758a59825b27b270bd7e14
josealejandro4343 <130449053+josealejandro4343@users.noreply.github.com>
2023-10-19 22:13:40 +0800

    added psychological form to create json

--- dashlabs/document_processor.py
+++ dashlabs/document_processor.py
@@ -47,24 +47,43 @@ class DocumentProcessor:
             target_folder = self.get_target_folder(form_name)
 
             if target_folder:
-                return result, target_folder
-        return None, None
+                return result, target_folder, form_name
+        return None, None, None
 
-    def create_json(self, target_folder, file_name, result):
+    def create_json(self, target_folder, file_name, result, form_name):
         file_output_folder = os.path.join(self.output_folder, target_folder)
         os.makedirs(file_output_folder, exist_ok=True)
         output_file = os.path.join(file_output_folder, f"{os.path.splitext(file_name)[0]}.json".lower())
-        
+        field_names_to_process = ["sense_of_responsibility", "emotional_stability", "objectivity", "motivation", "interpersonal_personal_adjustment", "goal_orientation"]  # Add your desired field names here
         data_dict = {}
-        for document in result.documents:
-            for name, field in document.fields.items():
-                field_value = field.value if field.value else field.content
-                data_dict[name] = field_value
+
+        if "psychological evaluation form" in form_name.lower():
+            for idx, document in enumerate(result.documents):
+                for name, field in document.fields.items():
+                    if name in field_names_to_process:
+                        if isinstance(field.value, dict):
+                            for row_key, row_value in field.value.items():
+                                if 'COLUMN1' in row_value.value and any(cell.value == 'selected' for cell in row_value.value.values()):
+                                    column1_value = row_value.value['COLUMN1'].value
+                                    for column, cell in row_value.value.items():
+                                        if cell.value == 'selected':
+                                            data_dict[column1_value] = column
+
+                    else:
+                        if isinstance(field.value, dict):
+                            for key, value in field.value.items():
+                                data_dict[key] = value.get('value') if isinstance(value, dict) else value
+                        else:
+                            data_dict[name] = field.value
         
+        else:
+            for document in result.documents:
+                for name, field in document.fields.items():
+                    field_value = field.value if field.value else field.content
+                    data_dict[name] = field_value
+
         with open(output_file, "w") as json_file:
             json.dump(data_dict, json_file, indent=4, cls=AzureJSONEncoder)
-
-
         
         print(f"Processed {file_name}. Recognized text saved to {output_file} in folder {target_folder}")
 
@@ -82,9 +101,9 @@ class DocumentProcessor:
                 file_path: {file_path}
             """)
             
-            result, target_folder = self.process_document(file_path, document_analysis_client, self.output_folder)
+            result, target_folder, form_name = self.process_document(file_path, document_analysis_client, self.output_folder)
             
             if result:
-                self.create_json(target_folder, file_name, result)
+                self.create_json(target_folder, file_name, result, form_name)
         
         print("Processing complete")
