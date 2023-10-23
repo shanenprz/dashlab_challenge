ea32a581fbf35d61cb41108ab15bf34bf301c05c
jlozion026 <jlozion@gmail.com>
2023-10-23 21:44:57 +0800

    uncomment JsonToCsvCombiner in main.py

--- main.py
+++ main.py
@@ -9,20 +9,20 @@ import numpy as np
 document_processor = DocumentProcessor()
 document_processor.process_and_save_documents()
 
-# # Define the target folders and output folder
-# target_folders = [
-#     "hiv_cert",
-#     "med_exam_landbase",
-#     "med_exam_seafarers",
-#     "med_cert_landbase",
-#     "med_cert_seafarers",
-# ]
-# output_csv_folder = "output_uncleaned_csv"
+# Define the target folders and output folder
+target_folders = [
+    "hiv_cert",
+    "med_exam_landbase",
+    "med_exam_seafarers",
+    "med_cert_landbase",
+    "med_cert_seafarers",
+]
+output_csv_folder = "output_uncleaned_csv"
 
 
-# # Create an instance of the class and call the method
-# combiner = JsonToCsvCombiner(target_folders, output_csv_folder)
-# combiner.combine_json_to_csv()
+# Create an instance of the class and call the method
+combiner = JsonToCsvCombiner(target_folders, output_csv_folder)
+combiner.combine_json_to_csv()
 
 
 
