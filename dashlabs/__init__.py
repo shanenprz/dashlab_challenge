b962ad55ca6c3a5f7c0404938ef7eea256082e4d
‎ <shanenprz@gmail.com>
2023-10-29 21:41:00 +0800

    Update __init__.py

--- dashlabs/__init__.py
+++ dashlabs/__init__.py
@@ -1,3 +1,2 @@
 from .document_processor import DocumentProcessor
-from .combine_to_csv import JsonToCsvCombiner
-from .data_cleaning import DataCleaning
+from .column_cleaning import ColumnCleaning
